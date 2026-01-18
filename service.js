afa4e79a0cc5c06dfd8b62a71e75dde9b19d15ae
buatforward-alt <buatforward@gmail.com>
2026-01-18 13:55:57 +0700

    Add files via upload

---
+++ service.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'ninepacman-v-final';
+const ASSETS = [
+    './',
+    './index.html',
+    './logo.png',
+    './bg.png'
+];
+
+// Install
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(ASSETS);
+        })
+    );
+});
+
+// Activate
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then(keys => Promise.all(
+            keys.map(key => {
+                if (key !== CACHE_NAME) return caches.delete(key);
+            })
+        ))
+    );
+    self.clients.claim();
+});
+
+// Fetch Strategy
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        fetch(event.request).catch(() => caches.match(event.request))
+    );
+});
